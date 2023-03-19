5d8d0fa72c1190518aaee03d7cddec4db71f1fdd
coderman47 <askinglucho@gmail.com>
2023-03-19 11:00:28 -0300

    create: create controllers for Products

--- src/routes/controllers/productRouter.js
+++ src/routes/controllers/productRouter.js
@@ -5,13 +5,13 @@ const router = Router();
 
 router.get("/", async (req, res) => {
   try {
-    const products = Product.findAll();
-    if (products === undefined) {
-      console.log("Entre por si no hay products");
-      throw new Error("No hay productos encontrados");
+    const products = await Product.findAll();
+
+    if (!products) {
+      res.status(404).send({ error: "No hay productos encontrados" });
+    } else {
+      res.status(200).send(products);
     }
-    console.log("PRODUCTS", products);
-    res.status(200).send(products);
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
@@ -20,27 +20,62 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const { name, description, price, stock, img, category } = req.body;
-    console.log("VIENE POR BODY", req.body);
     if (name && description && price && stock && img && category) {
       const createProduct = Product.create(req.body);
-      res
-        .status(200)
-        .send({ msg: `La categoria ${category} fue asignada satisfactoriamente`, createProduct });
+      res.status(200).send({
+        msg: `La categoria ${category} fue asignada satisfactoriamente`,
+        createProduct,
+      });
     } else {
-      throw new Error("Faltan campos para crear producto");
+      res.status(404).send({ error: "Faltan campos para crear producto" });
     }
   } catch (error) {
-    console.log(error);
     res.status(404).json({ error: error.message });
   }
 });
 
-// router.put("/updateProduct", async (req, res) => {
-//     try {
+router.put("/updateOne/:id", async (req, res) => {
+  const id = req.params.id;
+  const { name, description, stock, img, price, category } = req.body;
+  try {
+    const findProduct = await Product.findOne({ where: { id: id } });
 
-//     } catch (error) {
+    if (!findProduct) {
+      res.status(400).send({ error: "No se encontró producto con ese ID" });
+    }
+    const newProduct = await findProduct.update({
+      name,
+      description,
+      stock,
+      img,
+      price,
+      category,
+    });
+    if (newProduct) {
+      res.status(200).send(newProduct);
+    } else {
+      res.status(400).send({ error: "No se pudo actualizar correctamente" });
+    }
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
 
-//     }
-// });
+router.delete("/deleteOne/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const finded = await Product.findByPk(id);
+    if (finded) {
+      await Product.destroy({
+        where: { id: finded.id },
+      });
+      res.status(200).send("Producto eliminado correctamente");
+    } else {
+      res.status(400).send("El producto con ese ID no se encontró");
+    }
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
 
 module.exports = router;
