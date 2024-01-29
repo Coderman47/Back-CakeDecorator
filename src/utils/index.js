04a0f76adb4d0be2875a8a7d31d6b7874942c5cf
Julian Navarro <julianrodrigonavarro@gmail.com>
2024-01-29 17:01:39 -0300

    Update

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
-const HOST_FRONT = "http://localhost:3000";
-const HOST_BACK = "http://localhost:3001";
+// const HOST_FRONT = "http://localhost:3000";
+const HOST_FRONT =
+  "https://front-cake-decorator-ks1y-7ct5ipymb-julian-navarro.vercel.app";
+const HOST_BACK = "http://back-cake-decorator-web.onrender.com";
 
 module.exports = {
-    HOST_FRONT,
-    HOST_BACK
-}
+  HOST_FRONT,
+  HOST_BACK,
+};
